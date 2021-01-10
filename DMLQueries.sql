87de6d7a8de858d9b316f411f767dd82e6e0cf41
Kevin-Nguyen-Tran <39070708+Kevin-Nguyen-Tran@users.noreply.github.com>
2021-01-10 14:00:27 -0500

    Update to DML queries
    Since 2005 did not record the entire year of data (stopped at the end of Q2), the information and values would skew the data set. Needed to filter out data for 2005 and create analysis just for 2003 and 2004.

--- DMLQueries.sql
+++ DMLQueries.sql
@@ -1,36 +1,47 @@
 --DML Queries:
 
---What is the total number of units sold year over year?
+-- Average total units sold per year utilizing subqueries:
 
-SELECT order_date, year_id, SUM(quantity) AS total_units_sold FROM date_info
+SELECT year_id, AVG(total_units_sold) 
+FROM
+(SELECT order_date, year_id, SUM(quantity) AS total_units_sold FROM date_info
 JOIN order_info ON order_info.id = date_info.oi_id
 JOIN sales_info ON sales_info.oi_id = order_info.id
+WHERE year_id = 2003 OR year_id = 2004
+GROUP BY order_date, year_id
+)AS T
+GROUP BY year_id;
+-- 2003: 407.2 and 2004: 400.2
+
+-- What is the total sales revenue year over year by week?
+
+SELECT order_date, year_id, SUM(total_sales) AS total_sales_revenue FROM date_info
+JOIN order_info ON order_info.id = date_info.oi_id
+JOIN sales_info ON sales_info.oi_id = order_info.id
+WHERE year_id = 2003 OR year_id = 2004
 GROUP BY order_date, year_id
 ORDER BY order_date;
 
---What is the average total sales year over year?
+--Average total sales revenue by year?
 
-SELECT order_date, year_id, ROUND(AVG(total_sales), 2) AS avg_total_sales FROM date_info
+SELECT year_id, AVG(total_sales_revenue) 
+FROM
+(SELECT order_date, year_id, SUM(total_sales) AS total_sales_revenue FROM date_info
 JOIN order_info ON order_info.id = date_info.oi_id
 JOIN sales_info ON sales_info.oi_id = order_info.id
+WHERE year_id = 2003 OR year_id = 2004
 GROUP BY order_date, year_id
-ORDER BY order_date;
+)AS T
+GROUP BY year_id;
+--2003: $41,376 and 2004: $40,377
 
 --What time of year are our sales the highest?
 
 SELECT qtr_id, month_id, ROUND(SUM(total_sales), 2) AS total_sales FROM date_info
 JOIN order_info ON order_info.id = date_info.oi_id
 JOIN sales_info ON sales_info.oi_id = order_info.id
+WHERE year_id = 2003 OR year_id = 2004
 GROUP BY qtr_id, month_id
-ORDER BY ROUND(SUM(total_sales), 2) DESC;
--- Over the past 3 years, the total sales are the highest in Q4. Specifically in November generating $2,118,885.67 in total sales. 
-
---What was the gross revenue for each quarter between 2003-2005?
-
-SELECT qtr_id, ROUND(SUM(total_sales), 2) AS total_sales FROM date_info
-JOIN order_info ON order_info.id = date_info.oi_id
-JOIN sales_info ON sales_info.oi_id = order_info.id
-GROUP BY qtr_id
 ORDER BY ROUND(SUM(total_sales), 2) DESC;
 
 --What is the most popular type of product sold? (based on units sold)
